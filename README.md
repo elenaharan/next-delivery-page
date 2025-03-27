39985eaf0883eedca97c899e522548df016ee1b3
Elena Haran <elenaharan@gmail.com>
2025-03-27 15:40:48 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Next Delivery Page
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Next Delivery Page 🚚</h1>
+    </div>
+  );
+}
+
+export default App;
