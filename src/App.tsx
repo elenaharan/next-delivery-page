4ed71474feab680a92487eed1ee69a771a45193a
Elena Haran <elenaharan@gmail.com>
2025-03-27 16:18:39 +0000

    add and set up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-4xl font-bold text-center text-blue-500">
+        Tailwind is working!
+      </h1>
       <h1>Next Delivery Page 🚚</h1>
     </div>
   );
