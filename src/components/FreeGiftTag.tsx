3645c7f4e5ceb1d4b409d94711cee2e4743b1363
Elena Haran <elenaharan@gmail.com>
2025-03-28 11:22:01 +0000

    make free gift tag responsive

--- src/components/FreeGiftTag.tsx
+++ src/components/FreeGiftTag.tsx
@@ -1,7 +1,22 @@
-export default function FreeGiftTag(){
+export default function FreeGiftTag() {
   return (
-      <div className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 -rotate-12 bg-pink-300 text-xs text-purple-800 font-bold px-2 py-1 rounded-md shadow">
-        FREE GIFT
-      </div>
-    )
+    <div
+      className="
+        absolute
+        bg-pink-200 text-purple-800 font-bold px-4 py-1 shadow
+        text-xs md:text-sm
+        -bottom-4 md:bottom-auto
+        left-1/2 md:left-auto
+        -top-auto md:-top-2
+        -right-auto md:-right-4
+        transform
+        -translate-x-[50%] md:translate-x-0
+        rotate-[-5deg] md:rotate-6
+        block md:block
+      "
+      style={{ borderRadius: 0 }}
+    >
+      FREE GIFT
+    </div>
+  );
 }
